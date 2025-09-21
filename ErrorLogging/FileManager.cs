473543f6eb262cd194efd90b3bb1791196a6b1d2
Michael McKIE <michael_mckie@hotmail.com>
2025-09-21 11:57:16 +0800

    Add project files.

---
+++ ErrorLogging/FileManager.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace ErrorLogging;
+
+internal class FileManager {
+    private readonly Lock _fileLock = new();
+    private readonly int _retentionPeriod;
+    private readonly string _logsDirectory;
+
+    public FileManager (int logRetentionPeriod){
+        _retentionPeriod = logRetentionPeriod;
+
+        _logsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs");
+        CleanUpOldLogs(_retentionPeriod);
+    }
+
+    public void Log<TState> (LogLevel logLevel, TState state, Exception? exception, Func<TState, Exception?, string> formatter,
+                            [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0){
+        ArgumentNullException.ThrowIfNull(formatter);
+
+        var logMessage = BuildLogMessage(logLevel, state, exception, formatter, memberName, filePath, lineNumber);
+
+        WriteLogToFileAsync(logMessage);
+    }
+
+    private static string BuildLogMessage<TState>(LogLevel logLevel, TState state, Exception? exception, Func<TState, Exception?, string> formatter, string memberName, string filePath, int lineNumber){
+        return $"{DateTime.Now:dd-MM-yyyy HH:mm:ss} [{logLevel}] {formatter(state, exception)}" + $"\nSource: {Path.GetFileName(filePath)}.{memberName} Line: {lineNumber}";
+    }
+
+    private async void WriteLogToFileAsync (string logMessage){
+        try {
+            await Task.Yield();
+
+            lock (_fileLock){
+                LogToFile(logMessage);
+            }
+        } catch (Exception ex){
+            Debug.WriteLine($"Error writing to log file: {ex.Message}");
+        }
+    }
+
+    private void LogToFile (string logMessage){
+        try {
+            if (!Directory.Exists(_logsDirectory)){
+                Directory.CreateDirectory(_logsDirectory);
+            }
+
+            string filePath = Path.Combine(_logsDirectory, $"ApplicationLogs_{DateTime.Now:dd-MM-yyyy}.txt");
+            File.AppendAllText(filePath, logMessage + Environment.NewLine);
+        } catch (Exception ex){
+            Debug.WriteLine($"Error writing to log file: {ex.Message}");
+        }
+    }
+
+    private void CleanUpOldLogs (int retentionDays){
+        try {
+            if (Directory.Exists(_logsDirectory)){
+                var logFiles = Directory.GetFiles(_logsDirectory, "ApplicationLogs_*.txt");
+
+                foreach (var logFile in logFiles){
+                    DateTime creationDate = File.GetCreationTime(logFile);
+
+                    if (creationDate < DateTime.Now.AddDays(-retentionDays)){
+                        File.Delete(logFile);
+                    }
+                }
+            }
+        } catch (Exception ex){
+            Debug.WriteLine($"Error cleaning up old log files: {ex.Message}");
+        }
+    }
+}
---
+++ ErrorLogging/LoggingManager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ErrorLogging;
+
+public sealed class LoggingManager {
+    private const int DefaultRetentionPeriod = 7;
+    private static readonly Lazy<LoggingManager> _instance = new(() => new LoggingManager(DefaultRetentionPeriod));
+    private readonly FileManager _fileManager;
+    private readonly int _fileRetentionPeriod;
+
+    private LoggingManager (int fileRetentionPeriod){
+        _fileRetentionPeriod = fileRetentionPeriod < 0 ? DefaultRetentionPeriod : fileRetentionPeriod;
+        _fileManager = new FileManager(_fileRetentionPeriod);
+    }
+
+    public static LoggingManager Instance => _instance.Value;
+
+    public void LogInformation (string message){
+        _fileManager.Log(LogLevel.Information, message, null, (state, exception) => $"Message: {state}");
+    }
+
+    public void LogError (Exception ex, string message){
+        _fileManager.Log(LogLevel.Error, message, ex, (state, exception) => $"{state}: {exception?.Message}\nStackTrace: {exception?.StackTrace}");
+    }
+
+    public void LogWarning (string message){
+        _fileManager.Log(LogLevel.Warning, message, null, (state, exception) => $"Warning: {state}");
+    }
+}
